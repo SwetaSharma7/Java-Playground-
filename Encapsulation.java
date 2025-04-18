ca23904f2297a7cde436c7ae5ca434d533462bce
swetasharma <sweta.sharma@leapfinance.com>
2025-04-18 23:52:22 +0530

    static

---
+++ Encapsulation.java
@@ -0,0 +1,30 @@
+class Human {
+    private int age;
+    private String name;
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public void setAge(int a) {
+        this.age = a;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String args[]) {
+        Human human = new Human();
+        human.setAge(25);
+        human.setName("Sweta");
+
+        System.out.println(human.getName() + " " + human.getAge());
+    }
+}
