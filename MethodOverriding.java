20287f0b241acc0e195faa40a8f979c1cc81183b
swetasharma <sweta.sharma@leapfinance.com>
2025-04-20 15:01:22 +0530

    Method Overriding

---
+++ MethodOverriding.java
@@ -0,0 +1,19 @@
+class calc {
+        public int add(int a, int b) {
+            return a+b;
+        }
+    }
+
+    class advCalc extends calc{
+        public int add(int a, int b) {
+            return a+b;
+        }
+    }
+
+public class MethodOverriding {
+    public static void main(String args[]) {
+        advCalc obj = new advCalc();
+        int res = obj.add(5,5);
+        System.out.println(res);
+    }
+}
